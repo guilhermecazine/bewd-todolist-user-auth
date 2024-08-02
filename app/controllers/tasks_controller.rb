e6d51c75a43d0e580177cbe0ca63c9008283f467
guilhermecazine <guilhermecazine50@gmail.com>
2024-08-02 07:26:22 +0100

    tasks_controller.rb edited

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,29 +1,43 @@
-# app/controllers/tasks_controller.rb
 class TasksController < ApplicationController
-  before_action :authenticate_user!
-
   def index
-    @tasks = current_user.tasks
-    render json: { tasks: @tasks.as_json(only: [:id, :content, :completed, :created_at, :updated_at]) }
-  end
-
-  def create
-    @task = current_user.tasks.new(task_params)
-    if @task.save
-      render json: { task: @task.as_json(only: [:id, :content, :completed, :created_at, :updated_at]) }, status: :created
-    else
-      render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
-    end
+    @tasks = Task.all
+    render 'tasks/index' 
   end
 
   def index_by_current_user
-    @tasks = current_user.tasks
-    render json: { tasks: @tasks.as_json(only: [:id, :content, :completed, :created_at, :updated_at]) }
+    token = cookies.signed[:todolist_session_token]
+    session = Session.find_by(token: token)
+
+    if session
+      @tasks = session.user.tasks
+      render 'tasks/index'
+    else
+      render json: { tasks: [] }
+    end
+  end
+
+  def create
+    token = cookies.signed[:todolist_session_token]
+    session = Session.find_by(token: token)
+
+    if session
+      user = session.user
+      @task = user.tasks.new(task_params)
+
+      if @task.save
+        render 'tasks/create' 
+      else
+        render json: { success: false }
+      end
+    else
+      render json: { success: false }
+    end
   end
 
   def destroy
-    @task = current_user.tasks.find(params[:id])
-    if @task.destroy
+    @task = Task.find_by(id: params[:id])
+
+    if @task&.destroy
       render json: { success: true }
     else
       render json: { success: false }
@@ -31,40 +45,20 @@ class TasksController < ApplicationController
   end
 
   def mark_complete
-    @task = current_user.tasks.find(params[:id])
-    if @task.update(completed: true)
-      render json: { task: @task.as_json(only: [:id, :content, :completed, :created_at, :updated_at]) }
-    else
-      render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
-    end
+    @task = Task.find_by(id: params[:id])
+
+    render 'tasks/update' if @task&.update(completed: true)
   end
 
   def mark_active
-    @task = current_user.tasks.find(params[:id])
-    if @task.update(completed: false)
-      render json: { task: @task.as_json(only: [:id, :content, :completed, :created_at, :updated_at]) }
-    else
-      render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
-    end
+    @task = Task.find_by(id: params[:id])
+
+    render 'tasks/update' if @task&.update(completed: false)
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:content, :completed)
-  end
-
-  def authenticate_user!
-    token = cookies.signed[:todolist_session_token]
-    session = Session.find_by(token: token)
-    if session.nil?
-      render json: { success: false }, status: :unauthorized
-    else
-      @current_user = session.user
-    end
-  end
-
-  def current_user
-    @current_user
+    params.require(:task).permit(:content)
   end
 end
